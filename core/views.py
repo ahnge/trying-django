119e55a92c7bad94ab77df4c4f9eb40bf8499077
ahnge <ahnge226@gmail.com>
2022-11-03 17:14:55 +0630

    render data from the database

--- core/views.py
+++ core/views.py
@@ -1,10 +1,16 @@
 import random
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+from articles.models import Articles
 
 
 def home(request):
-    number = random.randint(1, 10000)
-    HTML_STRING = f"""
-    <h1>Hello world {number}</h1>
-    """
+    number = random.randint(1, 2)
+    article_obj = Articles.objects.get(pk=number)
+    article_list = Articles.objects.all()
+    context = {
+        "article_list": article_list
+    }
+    HTML_STRING = render_to_string("home-view.html", context=context)
     return HttpResponse(HTML_STRING)
