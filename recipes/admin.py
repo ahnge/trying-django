c0f6ffc1a209ecf94865c61066c51a96d404e7aa
ahnge <ahnge226@gmail.com>
2022-12-03 10:17:40 +0630

    testing the relationships

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient
 
 # Register your models here.
+class RecipeIngredientAdmin(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientAdmin]
+    list_display = ["name", "user"]
+    readonly_fields = ["created_at", "updated_at"]
+    raw_id_fields = ["user"]
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient)
